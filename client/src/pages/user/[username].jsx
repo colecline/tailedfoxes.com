df2e46ace7e47b11cfe4fa9b1dd9145a4afaecd5
Cole Cline <me@colecline.com>
2022-10-24 18:24:27 -0500

    Updated User Page

--- client/src/pages/user/[username].jsx
+++ client/src/pages/user/[username].jsx
@@ -1,7 +1,18 @@
-export default function UserPage() {
+function UserPage(user) {
     return (
         <>
-        User Page
+        Hello {user.username}!
         </>
     )
 }
+
+UserPage.getInitialProps = async (context) => {
+    const { username } = context.query;
+    
+    const res = await fetch(`https://api.tailedfoxes.com/user/${username}`);
+    const json = await res.json();
+
+    return json;
+}
+
+export default UserPage;
